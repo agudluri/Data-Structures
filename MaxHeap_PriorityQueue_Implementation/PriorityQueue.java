bce66a028032c6a38548cfbe51e93f7706cf5785
Aman Gudluri <amangudluri@yahoo.com>
2017-04-04 22:17:35 -0500

    Update PriorityQueue.java

--- MaxHeap_PriorityQueue_Implementation/PriorityQueue.java
+++ MaxHeap_PriorityQueue_Implementation/PriorityQueue.java
@@ -1,16 +1,14 @@
-import java.util.*;
+public class PQ<E> {
 
-public class PriorityQueue<E> {
-	
-	private transient Object[] queue;
-	private int size = 0;
-	private Comparator<? super E> comparator = null;
+	private transient E[] queue;
+	private int currentsize = 0;
+	private Comparator<E> comparator = null;
 
 	/**
 	 * Creates a PQ with the default initial capacity (11) that orders its
 	 * elements according to their natural ordering (Using Comparable).
 	 */
-	public CS401PQ() {
+	public PQ() {
 		this(11, null);
 	}
 
@@ -26,10 +24,10 @@ public class PriorityQueue<E> {
 	 * @throws IllegalArgumentException
 	 *             if initialCapacity is less than 1
 	 */
-	public CS401PQ(int initialCapacity, Comparator<? super E> comparator) {
+	public PQ(int initialCapacity, Comparator<E> comparator) {
 		if (initialCapacity < 1)
 			throw new IllegalArgumentException();
-		queue = new Object[initialCapacity];
+		queue = (E[]) new Object[initialCapacity];
 		this.comparator = comparator;
 	} // constructor
 
@@ -47,14 +45,19 @@ public class PriorityQueue<E> {
 	public boolean add(E e) {
 		if (e == null)
 			throw new NullPointerException();
-		int i = size;
-		if (i >= queue.length)
-			grow(i + 1);
-		size = i + 1;
-		if (i == 0)
+
+		int tempsize = currentsize;
+
+		if (tempsize >= queue.length)
+			grow(tempsize + 1);
+
+		currentsize = tempsize + 1;
+
+		if (tempsize == 0)
 			queue[0] = e;
 		else
-			siftUp(i, e);
+			siftUp(tempsize, e);
+
 		return true;
 	}
 
@@ -69,10 +72,10 @@ public class PriorityQueue<E> {
 	 *
 	 */
 	public E remove() {
-		if (size == 0)
+		if (currentsize == 0)
 			throw new NoSuchElementException();
 
-		int s = --size;
+		int s = --currentsize;
 
 		E elem = (E) queue[0];
 		E last = (E) queue[s];
@@ -87,7 +90,7 @@ public class PriorityQueue<E> {
 	public String toString() {
 		String s = "";
 
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < currentsize; i++)
 			s += queue[i] + " ";
 
 		return s;
@@ -105,7 +108,7 @@ public class PriorityQueue<E> {
 		if (minCapacity < 0) // overflow
 			throw new OutOfMemoryError();
 		int oldCapacity = queue.length;
-		// Double size if small; else grow by 50%
+		// Double current size if small; else grow by 50%
 		int newCapacity = ((oldCapacity < 64) ? ((oldCapacity + 1) * 2) : ((oldCapacity / 2) * 3));
 		if (newCapacity < 0) // overflow
 			newCapacity = Integer.MAX_VALUE;
@@ -125,17 +128,33 @@ public class PriorityQueue<E> {
 	 * @param x
 	 *            the item to insert
 	 */
-	private void siftUp(int k, E x) {
-		Comparable<? super E> key = (Comparable<? super E>) x;
-		while (k > 0) {
-			int parent = (k - 1) >>> 1;
-			Object e = queue[parent];
-			if (key.compareTo((E) e) <= 0)
-				break;
-			queue[k] = e;
-			k = parent;
+	private void siftUp(int n, E x) {
+		while (n > 0 && condition((n - 1) >>> 1, x)) {
+			int parent = (n - 1) >>> 1;
+			queue[n] = queue[parent]; // swap
+			n = parent;
 		}
-		queue[k] = key;
+		queue[n] = x;
+	}
+
+	/**
+	 * A support method for the SiftUp method for checking the condition between
+	 * a node and its parent
+	 * 
+	 * @param parentindex,
+	 *            the index of the nodes parent
+	 * @param x,
+	 *            the element to be inserted
+	 * @return a boolean denoting the correctness of the condition
+	 */
+	@SuppressWarnings("unchecked")
+	private boolean condition(int parentindex, E x) {
+		if (comparator == null) {
+			return ((Comparable<E>) queue[parentindex]).compareTo(x) < 0;
+		} else {
+			return comparator.compare(queue[parentindex], x) < 0;
+		}
+
 	}
 
 	/**
@@ -151,19 +170,19 @@ public class PriorityQueue<E> {
 	 *
 	 */
 	private void siftDown(int k, E x) {
-		Comparable<? super E> key = (Comparable<? super E>) x;
-		int half = size >>> 1; // loop while a non-leaf
+		Comparable<E> key = (Comparable<E>) x;
+		int half = currentsize >>> 1; // loop while a non-leaf
 		while (k < half) {
 			int child = (k << 1) + 1; // assume left child is least
-			Object c = queue[child];
+			E c = queue[child];
 			int right = child + 1;
-			if (right < size && ((Comparable<? super E>) c).compareTo((E) queue[right]) > 0)
+			if (right < currentsize && ((Comparable<E>) c).compareTo((E) queue[right]) < 0)
 				c = queue[child = right];
 			if (key.compareTo((E) c) >= 0)
 				break;
 			queue[k] = c;
 			k = child;
 		}
-		queue[k] = key;
+		queue[k] = x;
 	}
 }
